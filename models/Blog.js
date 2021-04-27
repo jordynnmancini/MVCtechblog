0b682fb5bf99f0b02319864bc4d0e78739bdc10c
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-27 11:26:43 -0400

    updated connection.js, controllers/index.js, schema, and started Blog & User Models / added env

---
+++ models/Blog.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
