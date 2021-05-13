6b5332fb48303162dfe0ca3967e3d2c3b8cef69a
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-05-12 20:55:26 -0400

    add blog from homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -11,7 +11,30 @@ function closeForm() {
         .classList.add('hidden');
 };
 
-// post request for a new blog 
+// create a new blog 
+const createNewBlog = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input-home').value.trim(); 
+    const text = document.querySelector('#text-input-home').value.trim();
+
+    
+    if (title && text) {
+        const response = await fetch('/api/blog', {
+          method: 'POST',
+          body: JSON.stringify({ title, text }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.reload(); 
+        } else {
+          alert('Failed to create blog post');
+        }
+    }
+};
 
 
 // Event Listeners 
@@ -22,5 +45,9 @@ document
 document
     .querySelector('#close-form-btn')
     .addEventListener('click', closeForm);
+
+document
+    .querySelector('#submit-blog-home')
+    .addEventListener('click', createNewBlog);
 
 
