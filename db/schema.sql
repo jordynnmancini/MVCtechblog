63592ff0e39bb200548efbe08fffc9bd3e759200
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-05-15 18:17:12 -0400

    connect to jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS techblog_db;
-CREATE DATABASE techblog_db;
+-- DROP DATABASE IF EXISTS techblog_db;
+-- CREATE DATABASE techblog_db;
 
 DROP DATABASE IF EXISTS tu4j0moodwsbylle;
 CREATE DATABASE tu4j0moodwsbylle;
